b666281fd45bf69488a8c9cc24f4271f2210770c
Roberto A. Zavala <112892402+QuiahEditorial@users.noreply.github.com>
2022-11-26 19:18:49 -0600

    Update termostato.ino

--- termostato.ino
+++ termostato.ino
@@ -1,46 +1,46 @@
-int Tr = 2;                   // puerto del transistor
-int Ra = 9950;                // resistencia de divisor de voltaje
-int Tm = A0;                  // puerto del termistor
+int   Tr = 2;                   // puerto del transistor
+int   Tm = A0;                  // puerto del termistor
 
-float Aa = 0.001482484340;    // Coefientes de Steinhart-Hart
-float Ba = 0.000167799949;
-float Ca = 0.000000406675;
+float A  = 0.001482484340;      // Coefientes de Steinhart-Hart
+float B  = 0.000167799949;
+float C  = 0.000000406675;
+int   R  = 9950;                // resistencia de divisor de voltaje
+float RT;                       // resistencia del transistor
+float t;                        // temperatura
 
-float T=0;
-float Ta;
-float Tb;
-float Tc;
+float Vs;                       // voltaje de salida 
+int   Ve = 5;                   // voltaje de entrada
 
-float RT;                     // resistencia del transistor
+int   PT;                       // PWM aplicado al transistor
+float To = 50;                  // temperatura objetivo
 
-float Vsa;                    // voltaje de salida 
-int   Ve = 5;                 // voltaje de entrada
-
-int   PT;                     // PWM aplicado al transistor
-float To;                     // temperatura objetivo
-
+int   dl = 10000;               // periodo de la rutina
 
 void setup()
   {
+  Serial.begin(9600);
   pinMode( Tr,OUTPUT );
+  Serial.println("Termostato :)");
   }
 
 void loop()
-  {
-  t = 0;                                           // Temperatura
-  for (int i=0; i <= 9; i++)
+{
+  t = 0;
+  for (int i=0; i <= 9; i++)    // se mide 10 veces la temperatura
     {
     Vs = (analogRead(Tra)/204.6);
     RT = (Ra*Vsa)/(Ve-Vsa);
-    dT = Aa + Ba*log(RTa) + Ca*pow(log(RTa),3);
+    dT = Aa + Ba * log( RT ) + Ca * pow( log(RT) , 3 );
     T  = 1/dT;
     T  = T - 273.15;
     t = t + T; 
     delay(200);
     }
-  t = t/10;
+  t = t/10;                     // obtiene la temperatura
 
   if      ( t <  To ) { analogWrite ( Tr , PT ); PT = PT + 1; }  // si t < To aumenta la potencia
-  else if ( t == To ) { analogWrite ( Tr , PT ); }               // si t = To se mantiene la potenica
+  else if ( t == To ) { analogWrite ( Tr , PT ); }               // si t = To se mantiene la potencia
   else                { analogWrite ( Tr, 0 );  PT = PT - 1; }   // si t > To se apaga y reduce en 1 la potencia
+  
+  delay();
 }
