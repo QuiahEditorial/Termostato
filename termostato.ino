78f4f60e86a9c43212db6d4ec75d125feb341e39
Roberto A. Zavala <112892402+QuiahEditorial@users.noreply.github.com>
2022-11-26 19:08:03 -0600

    Update termostato.ino

--- termostato.ino
+++ termostato.ino
@@ -16,6 +16,10 @@ float RT;                     // resistencia del transistor
 float Vsa;                    // voltaje de salida 
 int   Ve = 5;                 // voltaje de entrada
 
+int   PT;                     // PWM aplicado al transistor
+float To;                     // temperatura objetivo
+
+
 void setup()
   {
   pinMode( Tr,OUTPUT );
@@ -36,27 +40,7 @@ void loop()
     }
   t = t/10;
 
-  if      ( Ta <  To ) { analogWrite (Tta,round(PTa)); PTa = PTa + 1;}
-  else if ( Ta == To ) { analogWrite (Tta,PTa); }
-  else                 { analogWrite (Tta, 0 );  PTa = PTa - 1;}
-
-  Tc = 0;
-for (int k=0; k <= 9; k++)
-  {
-  Vsc = (analogRead(Trc)/204.8);
-  RTc = (Rc*Vsc)/(Ve-Vsc);
-  dTc = Ac + Bc*log(RTc) + Cc*pow(log(RTc),3);
-  tc  = 1/dTc;
-  T   = tc-273.15;
-  Tc  = T + Tc;
-  delay(200);
-  }
-
-if      ( Tb < 45 ) { analogWrite (Ttc,round(PTc)); PTc = PTc + 1;}
-else if ( Tb = 45 ) { analogWrite (Ttc,PTc); }
-else                { digitalWrite(Ttc,0 );  PTc = PTc -1;}
-
-if ( x % 2 ) { lcd.setCursor(0,1); lcd.write("C "); lcd.print(Tc,1);}
-else         { lcd.setCursor(2,1); lcd.print(int(PTc)); lcd.print("  ");}
-
+  if      ( t <  To ) { analogWrite ( Tr , PT ); PT = PT + 1; }  // si t < To aumenta la potencia
+  else if ( t == To ) { analogWrite ( Tr , PT ); }               // si t = To se mantiene la potenica
+  else                { analogWrite ( Tr, 0 );  PT = PT - 1; }   // si t > To se apaga y reduce en 1 la potencia
 }
